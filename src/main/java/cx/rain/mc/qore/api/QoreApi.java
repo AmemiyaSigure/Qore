eb4179db386ff6ed25511f6a014e08b556d6b420
秋雨落 <i@rain.cx>
2021-05-06 09:01:13 +0800

    :fire: 放弃Qore的database功能
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/qore/api/QoreApi.java
+++ src/main/java/cx/rain/mc/qore/api/QoreApi.java
@@ -1,19 +1,16 @@
 package cx.rain.mc.qore.api;
 
 import cx.rain.mc.qore.api.config.QoreConfig;
-import cx.rain.mc.qore.api.database.QoreDatabase;
 import org.bukkit.plugin.Plugin;
 
 public class QoreApi {
     private final Plugin plugin;
 
     private QoreConfig config;
-    private QoreDatabase database;
 
     public QoreApi(Plugin pluginIn) {
         plugin = pluginIn;
         config = new QoreConfig(this);
-        database = new QoreDatabase(this);
 
         Qore.add(plugin, this);
     }
@@ -24,9 +21,5 @@ public class QoreApi {
 
     public QoreConfig getConfig() {
         return config;
-    }
-
-    public QoreDatabase getDatabase() {
-        return database;
     }
 }
